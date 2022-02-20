6aaf5b1d5385533802445b5abd63073134f66001
yldz1911@gmail.com <1991erzurum25>
2022-02-20 13:28:05 +0300

    -Migrated to SplashScreen API and this used instead of SplashFragment -SplashFragment deleted.

--- app/src/main/java/com/mesutemre/kutuphanem/MainActivity.kt
+++ app/src/main/java/com/mesutemre/kutuphanem/MainActivity.kt
@@ -5,16 +5,17 @@ import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.splashscreen.SplashScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.google.android.material.bottomnavigation.LabelVisibilityMode
 import com.mesutemre.kutuphanem.databinding.ActivityMainBinding
-import com.mesutemre.kutuphanem.util.CustomSharedPreferences
-import com.mesutemre.kutuphanem.util.WRITE_EXTERNAL_STORAGE_REQUEST_CODE
+import com.mesutemre.kutuphanem.util.*
 import com.mesutemre.kutuphanem.util.customcomponents.CurvedBottomNavigationView
-import com.mesutemre.kutuphanem.util.hideComponent
-import com.mesutemre.kutuphanem.util.showComponent
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -28,9 +29,22 @@ class MainActivity : AppCompatActivity() {
     private lateinit var navBottomMenu:CurvedBottomNavigationView
     private var writeExternalStorageIzin:Int = 0
     private lateinit var navHostFragment:NavHostFragment
+    private var keepSplash:Boolean = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
+        var token:String? = null
+        splashScreen.setKeepOnScreenCondition(object :SplashScreen.KeepOnScreenCondition{
+            override fun shouldKeepOnScreen(): Boolean {
+                return keepSplash
+            }
+        })
+        lifecycleScope.launch {
+            token = customSharedPreferences.getStringFromSharedPreferences(APP_TOKEN_KEY)
+            keepSplash = false
+        }
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
@@ -38,7 +52,20 @@ class MainActivity : AppCompatActivity() {
         if(writeExternalStorageIzin != PackageManager.PERMISSION_GRANTED){
             requestPermissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), WRITE_EXTERNAL_STORAGE_REQUEST_CODE)
         }
+
         this.initNavBottomMenu()
+
+        if(token == null || token?.length == 0){
+            /*navHostFragment.navController.navigate(R.id.loginFragment,null, navOptions {
+                this.anim {
+                    this.popEnter = R.anim.slide_down
+                    this.enter = R.anim.slide_down
+                }
+            })*/
+            navHostFragment.navController.navigate(R.id.loginFragment)
+        }else {
+            navHostFragment.navController.navigate(R.id.anasayfaFragment)
+        }
 
         binding.floatingActionButton3.setOnClickListener {
             navHostFragment.navController.navigate(R.id.kitapEklemeFragment)
@@ -62,7 +89,6 @@ class MainActivity : AppCompatActivity() {
                     R.id.kitapEklemeFragment,
                     R.id.kitapDetayFragment,
                     R.id.kitapDetayDeepFragment,
-                    R.id.splashFragment,
                     R.id.loginFragment,
                     R.id.profilIslemAdSoyadEpostaFragment,
                     R.id.profilIslemDogumTarCinsiyetFragment,
@@ -79,4 +105,5 @@ class MainActivity : AppCompatActivity() {
         }
         setupWithNavController(navBottomMenu,navHostFragment.navController)
     }
+
 }
